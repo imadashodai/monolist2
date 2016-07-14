53627269015b9d1bc78bd537960e3b4eb2be7972
shoday10 <imada@fiilse.com>
2016-07-14 12:43:05 +0000

    add have want button

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -7,12 +7,12 @@ class OwnershipsController < ApplicationController
     else
       @item = Item.find(params[:item_id])
     end
-
+    
     # itemsテーブルに存在しない場合は楽天のデータを登録する。
     if @item.new_record?
       # TODO 商品情報の取得 RakutenWebService::Ichiba::Item.search を用いてください
-      items = {}
-
+      items = RakutenWebService::Ichiba::Item.search(itemCode: params[:item_code])
+      
       item                  = items.first
       @item.title           = item['itemName']
       @item.small_image     = item['smallImageUrls'].first['imageUrl']
@@ -21,12 +21,16 @@ class OwnershipsController < ApplicationController
       @item.detail_page_url = item['itemUrl']
       @item.save!
     end
-
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
-    
-
+    if params[:type] == "Have"
+#      @item = Item.find(params[:type])
+      current_user.have(@item)
+    else
+#      @item = Item.find(params[:type])
+      current_user.want(@item)
+    end
   end
 
   def destroy
@@ -35,6 +39,11 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
+    if params[:id] == "Have it"
+      current_user.unhave(@item)
+    else
+      current_user.unwant(@item)
+    end
 
   end
 end
